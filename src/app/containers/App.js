a66ab5ace3381c4be7e718851abf1b1d24fbf35d
Bonface Mudiri <mudirib@gmail.com>
2017-09-16 14:39:59 +0300

    Update App

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { render } from  'react-dom';
+
+import { User } from './components/User';
+import { Main } from './components/Main';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       Hello WOrld
-      </div>
-    );
-  }
-}
+    constructor(){
+        super();
+        this.state = {
+            username: "Bonface"
+        };
+    }
+    changeUsername = (newName) => {
+        this.setState({
+            username: newName
+        });
+    }
+    render(){
+        return(
+            <div className="App container">
+                <Main changeUsername={this.changeUsername.bind(this)} />
+                <User username={this.state.username } />
+            </div>
+        );
+    };
 
+};
 export default App;
